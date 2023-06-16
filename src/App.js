8e117e9bfc4ac339bffc9173cb0cb97860f22ec5
pàshú chy <chypashu@gmail.com>
2023-06-16 14:55:54 +0545

    bug fixed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 // import 'react-toastify/dist/ReactToastfy.css'
-import 'react-toastify/dist/ReactToastify.css';
-import { ToastContainer, toast } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
+// import { ToastContainer, toast } from 'react-toastify';
 import AppRouting from './AppRouting';
 
 function App() {
   return (
     <div className="App">
     <AppRouting />
-    <ToastContainer />
+    {/* <ToastContainer /> */}
     </div>
   );
 }
